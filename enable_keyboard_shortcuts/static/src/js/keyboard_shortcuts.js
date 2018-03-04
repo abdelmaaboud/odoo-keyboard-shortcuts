3dd438ca5689e99498ac36557354bd55645dcf25
islam abdelmaaboud <islamabdelmaaboud@yahoo.com>
2018-03-04 14:56:01 +0200

    add new functions

--- enable_keyboard_shortcuts/static/src/js/keyboard_shortcuts.js
+++ enable_keyboard_shortcuts/static/src/js/keyboard_shortcuts.js
@@ -18,4 +18,50 @@ $.shortcut = function(key, callback, args) {
         }
     });
 };
+//done
+//New object by Ctrl + enter
+$.ctrl('13', function() {
+	$('.o_form_button_create').each(function() {
+		if($(this).parents('div:hidden').length == 0){
+			$(this).trigger('click');
+		}
+	});
+	$('.o_list_button_add').each(function() {
+		if($(this).parents('div:hidden').length == 0){
+			$(this).trigger('click');
+		}
+	});
+	$('.o-kanban-button-new').each(function() {
+		if($(this).parents('div:hidden').length == 0){
+			$(this).trigger('click');
+		}
+	});
+});
+//done
+//Edit the current object F2
+$.shortcut('113', function() {
+	$('.o_form_button_edit').each(function() {
+		if($(this).parents('div:hidden').length == 0){
+			$(this).trigger('click');
+		}
+	});
+});
+//done
+//Save the current object
+$.ctrl('S', function() {
+	var ok= true;
+	$('.o_form_button_save').each(function() {
+		if($(this).parents('div:hidden').length == 0){
+			$(this).trigger('click');
+			ok = false
+		}
+	});
+	if(ok){
+		$('.o_form_button_save').each(function() {
+		if($(this).parents('div:hidden').length == 0){
+			$(this).trigger('click');
+		}
+	});
+	}
 
+});
